5557168e5b35ae96e469ea5ca63f19755ba2b28f
Karn Saheb <hello@karn.io>
2025-03-02 01:55:01 -0800

    Version bump: 0.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "io.karn.prism"
         minSdk = 33
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "0.0.${versionCode}"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
