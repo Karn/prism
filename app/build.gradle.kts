16157b0cccb6a79b75240259d2eaffe1929a0da4
Karn Saheb <hello@karn.io>
2025-03-07 02:11:51 -0800

    Disable Google Signing Block: https://gitlab.com/fdroid/fdroiddata/-/merge_requests/20147#note_2384616635

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,21 @@
+/*
+ * Prism - Wallpaper Manager
+ * Copyright (C) 2025 Karn Saheb
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -57,6 +75,12 @@ android {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
+    }
+    dependenciesInfo {
+        // Disables dependency metadata when building APKs.
+        includeInApk = false
+        // Disables dependency metadata when building Android App Bundles.
+        includeInBundle = false
     }
 }
 
